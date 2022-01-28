4aac310a0be02618e4205b14aa91103c7e61a772
lejt <92820402+lejt@users.noreply.github.com>
2022-01-28 10:36:14 -0800

    router and Link added

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,9 +1,30 @@
 import './App.css';
+import React from "react";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import NewOrderPage from "../NewOrderPage/NewOrderPage";
+import AuthPage from "../AuthPage/AuthPage";
+import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
+import NavBar from "../../components/NavBar/NavBar";
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <main className="App">
-      App
+      {
+        user ?
+        <>
+          <NavBar />
+          <Routes>
+            {/* Future routes go here */}
+            <Route path="/orders/new" element={<NewOrderPage />} />
+            <Route path="/orders" element={<OrderHistoryPage />} />
+          </Routes>
+        </>
+        :
+        <AuthPage />
+      }
     </main>
   );
 }
---
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+export default function OrderHistoryPage() {
+    return (
+        <h1>Order History Page</h1>
+    )
+}
