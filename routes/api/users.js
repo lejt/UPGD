f88060be53913d154d96b06aba86324b1af5c321
lejt <92820402+lejt@users.noreply.github.com>
2022-02-01 16:13:45 -0800

    mern skeleton infrastructure

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const usersCtrl = require('../../controllers/api/users');
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
+
+// Insert ensureLoggedIn on all routes that need protecting
+router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken);
 
 // POST - '/api/users/'
 router.post('/', usersCtrl.create);
+
+// POST - '/api/users/login'
+router.post('/login', usersCtrl.login);
+
+// GET - '/api/users/check-token'
+router.get('/check-token', usersCtrl.checkToken);
+
 
 
 module.exports = router;
--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -35,3 +35,14 @@ export function getUser() {
 export function logOut() {
   localStorage.removeItem('token');
 }
+
+export async function login(credentials) {
+  const token = await usersAPI.login(credentials);
+  localStorage.setItem('token', token);
+  return getUser();
+}
+
+export async function checkToken() {
+  const dateStr = await usersAPI.checkToken();
+  return new Date(dateStr);
+}
